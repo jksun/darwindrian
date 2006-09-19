d6b7bfc9db5f65448e03974b27f4f3120ba65a1b
alienbat <alienbat@6deb261b-ae1c-0410-ac43-a12d6d7c63d6>
2006-09-19 03:04:02 +0000

    new event model

--- prototype_darwindrian.py
+++ prototype_darwindrian.py
@@ -73,7 +73,8 @@ class HVPoint(awt.Point):
 			return 'Terminal'
 		elif len(self.__options) == 2:
 			self.__options.sort()
-			if self.__options <> ['East', 'West'] and self.__options <> ['North', 'South']:
+			if self.__options <> ['East', 'West'] \
+				and self.__options <> ['North', 'South']:
 				return 'RightAngle'
 			else:
 				return 'OnLine'
@@ -237,7 +238,7 @@ class Mondrian:
 #Seperate ------------------------------------
 #The following is Swing programming (low tech)
 
-class Canvas(swing.JPanel, awt.event.ActionListener):
+class Canvas(swing.JPanel):
 	def __init__(self):
 		swing.JPanel.__init__(self)
 		global mondrian_instance
@@ -300,40 +301,78 @@ class Canvas(swing.JPanel, awt.event.ActionListener):
 		
 	def __load_popup_menu(self):
 		self.__popup = swing.JPopupMenu()
-		self.__saveas = swing.JMenuItem('Save as png...')
-		self.__popup.add(self.__saveas)
-		self.__saveas.addActionListener(self)
+		self.__saveas_b = swing.JMenuItem('Save as png...')
+		self.__popup.add(self.__saveas_b)
 		
-		class MV(MouseAdapter):
-			def __init__(self, popup):
-				self.__popup = popup
-				
-			def mouseClicked(self, e):
-				if e.getButton() == MouseEvent.BUTTON3:
-					self.__popup.show(e.getComponent(), e.getX(), e.getY())
+		controller.add_mouse_action(self, self.__popingup, 'Clicked')
+		controller.add_action(self.__saveas_b, self.__save_as)
+		
+	def __popingup(self, e):
+		if e.getButton() == MouseEvent.BUTTON3:
+			self.__popup.show(e.getComponent(), e.getX(), e.getY())
 					
-		self.addMouseListener(MV(self.__popup))
-		
-	def actionPerformed(self, e):
-		if e.getSource() == self.__saveas:
-			self.__save_as()
-			
-class ControlPane(swing.JPanel,awt.event.ActionListener):
+class ControlPane(swing.JPanel):
 	def __init__(self):
 		swing.JPanel.__init__(self)
 		self.__next_b = swing.JButton('next')
-		self.__next_b.addActionListener(self)
+		#self.__next_b.addActionListener(self)
+		controller.add_action(self.__next_b, self.__next_paint)
 		self.layout = awt.FlowLayout(awt.FlowLayout.RIGHT)
 		self.add(self.__next_b)
+	
+	def __next_paint(self):
+		mondrian_instance.refresh()
+		self.getParent().repaint()
 		
-	def actionPerformed(self, e):
-		global mondrian_instance
-		if e.getSource() == self.__next_b:
-			mondrian_instance.refresh()
-			self.getParent().repaint()
+
+class GlobalController:
+	
+	class DummyAction(awt.event.ActionListener):
+		def __init__(self, target_method, parameters):
+			self.__target = target_method
+			self.__parameters = parameters
 			
+		def actionPerformed(self, e):
+			self.__target(*self.__parameters)
+	
+	class DummyMouseAction(MouseAdapter):
+		'''action type: Clicked/Entered/Exited/Pressed/Released'''
+		def __init__(self, target_method, action_type, parameters):
+			MouseAdapter.__init__(self)
+			self.__target = target_method
+			self.__action_type = action_type
+			self.__parameters = parameters
+			#Dynamic delegation
+			setattr(self, 'mouse'+action_type, self.__delegate_to)
+			
+		def __delegate_to(self, event):
+			self.__target(event, *self.__parameters)
+		
+		#Jython bug:
+		#static definition must present or dynamic delegation won't work
+		def mouseClicked(self, f):
+			raise 'should never be called'
+			
+	#Inner class definition done		
+			
+	def add_action(self, source, method, *para):
+		a = GlobalController.DummyAction(method, para)
+		source.addActionListener(a)
+		
+	def add_mouse_action(self, source, method, action_type, *para):
+		m = GlobalController.DummyMouseAction(method, action_type, para)
+		source.addMouseListener(m)
+		#test trigger
+		#m.mouseClicked(None)
+		
+	def __init__(self):
+		pass
+	
+	
+	
 #global singleton instance
 mondrian_instance = Mondrian()
+controller = GlobalController()
 
 def start_window():
 	root = swing.JFrame(title = 'Darwindrian - prototype')
@@ -356,7 +395,7 @@ def testing():
 			start_window()
 		except NullPointerException, e1:
 			print 'Warning: null pointer exception detected'
-		except Exception, e2:
-			print 'Warning: Exception detected'
+		#except Exception, e2:
+		#	print 'Warning: Exception detected:',e2
 #run
 testing()
