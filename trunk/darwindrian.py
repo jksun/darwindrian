668700b662d72c4532c8111ac70dfdc85c7b3665
alienbat <alienbat@6deb261b-ae1c-0410-ac43-a12d6d7c63d6>
2006-10-06 05:26:04 +0000

    structure refactory

---
+++ trunk/darwindrian.py
@@ -0,0 +1,41 @@
+from prototype_darwindrian import *
+from darwindrian_ui import *
+
+#Assemble gui elements
+def start_window():
+	root = swing.JFrame(title = 'Darwindrian - prototype')
+	content = swing.JPanel(layout = awt.BorderLayout())
+	content.add(gui_canvas, awt.BorderLayout.CENTER)
+	content.add(gui_control, awt.BorderLayout.SOUTH)
+	
+	#Issue split bar
+	split = swing.JSplitPane(swing.JSplitPane.HORIZONTAL_SPLIT)
+	split.setLeftComponent(gui_miniView)
+	split.setRightComponent(content)
+	
+	#Issue whole arrangment
+	top = swing.JPanel(layout = awt.BorderLayout())
+	
+	top.add(split, awt.BorderLayout.CENTER)
+	top.add(gui_statusBar, awt.BorderLayout.SOUTH)
+	top.add(gui_menu, awt.BorderLayout.NORTH)
+	
+	root.contentPane = top
+	root.visible = 1
+	root.resizable = 0
+	root.defaultCloseOperation = swing.JFrame.EXIT_ON_CLOSE
+	root.pack()
+	
+	#generate first Mondrian graph
+	gui_control.next_paint()
+	
+def testing():
+	if __name__ == '__main__':
+		#try:
+		start_window()
+		#except NullPointerException, e1:
+		#	print 'Warning: null pointer exception detected'
+		#except Exception, e2:
+		#	print 'Warning: Exception detected:',e2
+#run
+testing()
